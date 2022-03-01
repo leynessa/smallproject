b6c40b1fe3fc2395f02ebdf59fdc379ebc11e02d
ashley williams <leynessa1@gmail.com>
2022-02-28 22:14:35 -0300

    hangman game

---
+++ hang_man/hangman.rb
@@ -0,0 +1,77 @@
+require_relative 'interface'
+class Hangman
+  def initialize
+    @word = words.sample
+    @lives = 7
+    @word_teaser = ""
+    @word.first.size.times do
+      @word_teaser += "_ "
+    end
+  end
+
+  def words
+    [
+      ["cricket", "a game played by men in all white"],
+      ["jogging", "people do this to loose weight"],
+      ["water", "you cant live without this beverage"],
+      [ "apple", "this keeps the doctor away"],
+      ["ruby", "objective programming language"]
+    ]
+  end
+
+  def print_teaser(last_guess = nil)
+    update_teaser(last_guess) unless last_guess.nil?
+    puts @word_teaser
+  end
+
+  def update_teaser(last_guess)
+    new_teaser = @word_teaser.split
+
+    new_teaser.each_with_index do |letter, index|
+      if letter == '_' && @word.first[index] == last_guess
+        new_teaser[index] = last_guess
+      end
+    end
+
+    @word_teaser = new_teaser.join(' ')
+  end
+
+  def make_guess
+    if @lives > 0
+      puts "Enter a letter"
+      guess = gets.chomp
+      good_guess = @word.first.include? guess
+      if guess == "exit"
+        puts "Thank you for playing!"
+      elsif guess.length > 1
+        puts "only guess 1 letter "
+          make_guess
+      elsif good_guess
+        puts "You are correct!"
+        print_teaser(guess)
+        if @word.first == @word_teaser.split.join
+          puts "Congratulations! you are a winner"
+        else
+          make_guess
+        end
+      else
+        @lives -= 1
+        puts "Sorry... you have #{@lives} lives left. Try again!"
+        make_guess
+      end
+    else
+      puts "Game over! you loose!"
+    end
+  end
+
+  def begin
+    puts "your word is #{@word.first.size} characters long"
+    puts "To exit game at any point type 'exit'"
+    print_teaser
+    puts "Clue: #{@word.last}"
+    make_guess
+  end
+end
+
+game = Hangman.new
+game.begin
